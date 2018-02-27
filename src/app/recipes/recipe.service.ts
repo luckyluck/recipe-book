8a5fea556c64581d89888fcffa51eb59a492d613
alexander_ketov <alexander_ketov@epam.com>
2018-02-27 10:56:10 +0600

    Add ingredients to recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import Recipe from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import Ingredient from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,20 @@ export class RecipeService {
         new Recipe(
             'A Test Recipe',
             'This is simply a test number one',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
         ),
         new Recipe(
             'Another Test Recipe',
             'This is simply a test number two',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]
         )
     ];
 
