6d82c8ab029502108a17d21ec1c4d3bbf9d861c1
alexander_ketov <alexander_ketov@epam.com>
2018-02-27 10:11:50 +0600

    Add recipe service and moved logic into it instead of one-way-binding.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
+import { RecipeService } from './../recipe.service';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import Recipe from 'app/recipes/recipe.model';
+import Recipe from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -9,23 +10,13 @@ import Recipe from 'app/recipes/recipe.model';
 export class RecipeListComponent implements OnInit {
     @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-    recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe',
-            'This is simply a test number one',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-        new Recipe(
-            'Another Test Recipe',
-            'This is simply a test number two',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        )
-    ];
+    recipes: Recipe[] = [];
 
-    constructor() {
+    constructor(private recipeService: RecipeService) {
     }
 
     ngOnInit() {
+        this.recipes = this.recipeService.getRecipes();
     }
 
     onRecipeSelected(recipe: Recipe) {
