8597259846a9cf3bf3ed9a8881e51ba8e55d6bd8
Alexander <ketov.alex.v@yandex.kz>
2018-02-28 22:19:05 +0600

    Add recipe edit empty component and finish with the routing.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import Recipe from '../recipe.model';
 import { RecipeService } from './../recipe.service';
@@ -11,21 +11,24 @@ import { RecipeService } from './../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
     recipe: Recipe;
-    id: number;
 
-    constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+    constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit() {
         this.route.params.subscribe(
             (params: Params) => {
-                this.id = +params['id'];
-                this.recipe = this.recipeService.getRecipe(this.id);
+                this.recipe = this.recipeService.getRecipe(+params['id']);
             }
         );
     }
 
     addToShoppingList() {
         this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    }
+
+    onEditRecipe() {
+        this.router.navigate(['edit'], {relativeTo: this.route});
+        // this.router.navigate(['../', this.recipe.id, 'edit'], {relativeTo: this.route});
     }
 
 }
