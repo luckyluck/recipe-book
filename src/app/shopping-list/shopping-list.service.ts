c131a1a575d817909631a253a9f10c3b8bc1adca
Alexander <ketov.alex.v@yandex.kz>
2018-03-02 22:51:52 +0600

    Replace emitters with rxjs Subjects.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,8 @@
-import { EventEmitter } from '@angular/core';
-
 import Ingredient from '../shared/ingredient.model';
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
-    ingredientChanged = new EventEmitter<Ingredient[]>();
+    ingredientChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('apples', 5),
@@ -17,12 +16,12 @@ export class ShoppingListService {
     addIngredient(ingredient: Ingredient) {
         // TODO add check on existing ingredient and sumarize amount insted of adding new record
         this.ingredients.push(ingredient);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
         // TODO add check on existing ingredient and sumarize amount insted of adding new record
         this.ingredients.push(...ingredients);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
     }
 }
