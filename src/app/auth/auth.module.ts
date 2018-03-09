794d785274159b6b2b2fa5285f081171cd42e091
Alexander <ketov.alex.v@yandex.kz>
2018-03-09 12:00:17 +0600

    Minor changes.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
@@ -8,7 +7,6 @@ import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   imports: [
-    CommonModule,
     FormsModule,
     AuthRoutingModule
   ],
